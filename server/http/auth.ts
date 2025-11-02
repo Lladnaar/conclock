43ca98c043761ff72678ac02a41da126e9229686
Lladnaar <mark.randall@email.com>
2025-11-02 21:30:29 +1300

    Stripped down user/auth

--- server/http/auth.ts
+++ server/http/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import auth from "basic-auth";
 import {UnauthorisedError} from "../http/error.ts";
-import {UserFactory} from "../resource/user.ts";
-import {TokenFactory, type TokenResource} from "../resource/token.ts";
+import * as data from "../data/redis.ts";
 import {StatusCodes as http} from "http-status-codes";
+import {randomBytes} from "node:crypto";
+import {userFind, checkPassword} from "../resource/user.ts";
 
 const BEARER_PREFIX = "Bearer ";
 
@@ -17,87 +18,108 @@ function cookieAuth(req: express.Request): string | undefined {
     return req.cookies["Session-Token"];
 }
 
-// Rest
-
 interface UserRequest extends express.Request {userId?: string}
 
-export class AuthRest {
-    userFactory = new UserFactory();
-    tokenFactory = new TokenFactory();
-
-    async authenticate(req: UserRequest, res: express.Response, next: express.NextFunction) {
-        const basicAuth = auth(req);
-        const tokenId = tokenAuth(req);
-        const cookieId = cookieAuth(req);
-        if (basicAuth) {
-            const userId = await this.userFactory.find("username", basicAuth.name);
-            if (userId && await this.userFactory.checkPassword(userId, basicAuth.pass)) {
-                req.userId = userId.id;
-                next();
-            }
-            else
-                throw new UnauthorisedError("Invalid credentials");
-        }
-        else if (tokenId) {
-            try {
-                const token = await this.tokenFactory.load(tokenId) as TokenResource;
-                req.userId = token.userId;
-                next();
-            }
-            catch {
-                throw new UnauthorisedError("Invalid credentials");
-            }
-        }
-        else if (cookieId) {
-            try {
-                const token = await this.tokenFactory.load(cookieId) as TokenResource;
-                req.userId = token.userId;
-                next();
-            }
-            catch {
-                throw new UnauthorisedError("Invalid credentials");
-            }
+async function authenticate(req: UserRequest, res: express.Response, next: express.NextFunction) {
+    const basicAuth = auth(req);
+    const tokenId = tokenAuth(req);
+    const cookieId = cookieAuth(req);
+    if (basicAuth) {
+        const userId = await userFind("username", basicAuth.name);
+        if (userId && await checkPassword(userId, basicAuth.pass)) {
+            req.userId = userId;
+            next();
         }
         else
-            throw new UnauthorisedError("No credentials");
+            throw new UnauthorisedError("Invalid credentials");
     }
-
-    async login(req: UserRequest, res: express.Response) {
-        let token: TokenResource;
-        const tokenId = await this.tokenFactory.find("userId", req.userId!);
-        if (tokenId)
-            token = await this.tokenFactory.load(tokenId.id) as TokenResource;
-        else
-            token = await this.tokenFactory.create({
-                userId: req.userId,
-            }) as TokenResource;
-
-        res
-            .status(http.OK)
-            .cookie("Session-Token", token.id)
-            .json(this.tokenFactory.toRest(token));
+    else if (tokenId) {
+        try {
+            const token = await getToken(tokenId) as TokenData;
+            req.userId = token.userId;
+            next();
+        }
+        catch {
+            throw new UnauthorisedError("Invalid credentials");
+        }
     }
-
-    async revoke(req: UserRequest, res: express.Response) {
-        const tokenId = await this.tokenFactory.find("userId", req.userId!);
-        if (tokenId)
-            await this.tokenFactory.delete(tokenId.id);
-
-        res
-            .status(http.NO_CONTENT)
-            .cookie("Session-Token", undefined)
-            .send();
+    else if (cookieId) {
+        try {
+            const token = await getToken(cookieId) as TokenData;
+            req.userId = token.userId;
+            next();
+        }
+        catch {
+            throw new UnauthorisedError("Invalid credentials");
+        }
     }
+    else
+        throw new UnauthorisedError("No credentials");
 }
 
-// Router
+type TokenData = {
+    userId: string;
+};
 
-const authRest = new AuthRest();
+type TokenResponse = {
+    sessionToken: string;
+} & Partial<TokenData>;
+
+function makeTokenResponse(id: string, token?: TokenData): TokenResponse {
+    const tokenResponse: TokenResponse = {
+        sessionToken: id,
+    };
+    if (token) {
+        tokenResponse.userId = token.userId;
+    }
+    return tokenResponse;
+}
+
+async function getToken(id: string): Promise<TokenData> {
+    return await data.get("token", id) as TokenData;
+}
+
+async function createToken(userId: string): Promise<string> {
+    const apiKey = randomBytes(32).toString("base64");
+    const token = {userId: userId};
+
+    await data.set("token", apiKey, token);
+    return apiKey;
+}
+
+async function findToken(userId: string): Promise<string | null> {
+    return await data.find("token", "userId", userId);
+}
+
+async function returnToken(req: UserRequest, res: express.Response) {
+    let token: TokenData;
+    let tokenId = await findToken(req.userId!);
+    if (tokenId)
+        token = await data.get("token", tokenId) as TokenData;
+    else {
+        tokenId = await createToken(req.userId!);
+        token = {userId: req.userId!};
+    }
+
+    res
+        .status(http.OK)
+        .cookie("Session-Token", tokenId)
+        .json(makeTokenResponse(tokenId, token));
+}
+
+async function revokeToken(req: UserRequest, res: express.Response) {
+    const tokenId = await findToken(req.userId!);
+    if (tokenId)
+        await data.del("token", tokenId);
+
+    res
+        .status(http.NO_CONTENT)
+        .cookie("Session-Token", undefined)
+        .send();
+}
 
 const router = express.Router();
-router.use("/", authRest.authenticate.bind(authRest));
-router.post("/", authRest.login.bind(authRest));
-router.get("/", authRest.login.bind(authRest));
-router.delete("/", authRest.revoke.bind(authRest));
+router.post("/", authenticate, returnToken);
+router.delete("/", authenticate, revokeToken);
 
 export default router;
--- server/resource/user.ts
+++ server/resource/user.ts
@@ -1,151 +1,154 @@
 import express from "express";
-import type {Id, Content} from "./resource.ts";
-import {ResourceFactory, InvalidResourceError} from "./resource.ts";
-import {Rest} from "../http/rest.ts";
 import {NotFoundError, BadRequestError} from "../http/error.ts";
 import bcrypt from "bcrypt";
 import * as data from "../data/redis.ts";
 import {StatusCodes as http} from "http-status-codes";
 
-// Types
-
-export type User = Content & {
+type UserData = {
+    name: string;
     username: string;
-    password: string | undefined;
+    password?: string;
 };
 
-export type UserResource = Id & User;
+function isValidUser(user: unknown): user is UserData {
+    return true; // **** FIX THIS ****
+}
 
-type Password = {
+type UserResponse = {
+    id: string;
+    url: string;
+    password?: {url: string};
+    token?: {url: string};
+} & Partial<Omit<UserData, "password">>;
+
+type PasswordData = {
     password: string;
     oldPassword?: string;
 };
 
-// Factory
+function isValidPassword(password: unknown): password is PasswordData {
+    return true; // **** FIX THIS ****
+}
 
-export class UserFactory extends ResourceFactory {
-    constructor() { super("user"); }
-
-    newContent(content: object): User {
-        if (!this.isValid(content)) throw new InvalidResourceError("Invalid user");
-
-        return {
-            ...super.newContent(content),
-            username: content.username,
-            password: content.password,
-        };
+function makeUserResponse(id: string, user?: UserData): UserResponse {
+    const userResponse: UserResponse = {
+        id: id,
+        url: `/api/user/${id}`,
+    };
+    if (user) {
+        userResponse.name = user.name;
+        userResponse.username = user.username;
+        userResponse.password = {url: `/api/user/${id}/password`};
+        userResponse.token = {url: `/api/user/${id}/token`};
     }
+    return userResponse;
+}
 
-    isValid(item: object): item is User {
-        let validity = super.isValid(item);
-        validity &&= "username" in item && typeof item.username === "string";
-        validity &&= !("password" in item) || ("password" in item && (typeof item.password === "string" || typeof item.password == "undefined"));
-        return validity;
+async function userGetAll(req: express.Request, res: express.Response) {
+    const idList = await data.list("user");
+    const userList = idList.map(id => makeUserResponse(id));
+    res.status(http.OK).json(userList);
+}
+
+async function userGet(req: express.Request, res: express.Response) {
+    const id = req.params.id!;
+    try {
+        const item = await data.get("user", id) as UserData;
+        const user = makeUserResponse(id, item);
+        res.status(http.OK).json(user);
     }
-
-    isValidPassword(item: object): item is Password {
-        let validity = true;
-        validity &&= "password" in item && typeof item.password === "string";
-        validity &&= !("oldPassword" in item) || ("oldPassword" in item && typeof item.oldPassword === "string");
-        return validity;
-    }
-
-    toData(item: User) {
-        return {
-            ...super.toData(item),
-            username: item.username,
-            password: item.password,
-        };
-    }
-
-    toRest(item: Id) {
-        if (this.isValid(item))
-            return {
-                ...super.toRest(item),
-                password: {url: super.makeUrl([item.id, "password"])},
-            };
+    catch (error) {
+        if (error instanceof data.LookupError)
+            throw new NotFoundError(error.message);
         else
-            return super.toRest(item);
+            throw error;
     }
+}
 
-    async create(user: {password: string | undefined}) {
-        if (typeof user.password === "string")
-            user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
-        return super.create(user);
+async function userPost(req: express.Request, res: express.Response) {
+    if (isValidUser(req.body)) {
+        if (typeof req.body.password === "string")
+            req.body.password = await encryptPassword(req.body.password);
+
+        const id = await data.add("user", req.body as data.Data);
+        const user = makeUserResponse(id, req.body);
+        res.status(http.CREATED).json(user);
     }
+    else
+        throw new BadRequestError("Invalid user data.");
+}
 
-    async save(id: string, user: {password: string | undefined}) {
-        if (typeof user.password === "string")
-            user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
+async function userPut(req: express.Request, res: express.Response) {
+    if (isValidUser(req.body)) {
+        const id = req.params.id!;
+        if (typeof req.body.password === "string")
+            req.body.password = await encryptPassword(req.body.password);
         else {
-            const existingUser = await data.get(this.type, id);
-            user.password = typeof existingUser.password == "string" ? existingUser.password : undefined;
+            const existingUser = await data.get("user", id);
+            req.body.password = typeof existingUser.password == "string" ? existingUser.password : undefined;
         }
-        return super.save(id, user);
-    }
 
-    async setPassword(user: Id, password: string): Promise<void> {
-        const hash = await bcrypt.hash(password, await bcrypt.genSalt());
-        await data.update(this.type, user.id, {password: hash});
+        await data.set("user", id, req.body as data.Data);
+        const user = makeUserResponse(id, req.body);
+        res.status(http.OK).json(user);
     }
-
-    async checkPassword(user: Id, password: string): Promise<boolean> {
-        const userData = await data.get(this.type, user.id);
-        if ("password" in userData && typeof userData.password === "string")
-            return bcrypt.compare(password, userData.password);
-        else
-            return false;
-    }
-
-    async find(property: string, value: string) {
-        const userId = await data.find(this.type, property, value);
-        if (userId)
-            return this.newId(userId);
-        else
-            return null;
-    }
+    else
+        throw new BadRequestError("Invalid user data.");
 }
 
-// Rest
+async function userDelete(req: express.Request, res: express.Response) {
+    const id = req.params.id!;
+    await data.del("user", id);
+    res.status(http.NO_CONTENT).send();
+}
 
-class UserRest extends Rest {
-    userFactory: UserFactory;
-    constructor() {
-        const userFactory = new UserFactory();
-        super(userFactory);
-        this.userFactory = userFactory;
-    }
+async function encryptPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, await bcrypt.genSalt());
+}
 
-    async setPassword(req: express.Request, res: express.Response) {
-        try {
-            const id = this.factory.newId(req.params.id!);
-            if (this.userFactory.isValidPassword(req.body)) {
-                if ("oldPassword" in req.body && !await this.userFactory.checkPassword(id, req.body.oldPassword!))
-                    throw new BadRequestError("Old password incorrect");
-                else
-                    await this.userFactory.setPassword(id, req.body.password);
+export async function checkPassword(id: string, password: string): Promise<boolean> {
+    const userData = await data.get("user", id);
+    if ("password" in userData && typeof userData.password === "string")
+        return bcrypt.compare(password, userData.password);
+    else
+        return false;
+}
+
+async function userSetPassword(req: express.Request, res: express.Response) {
+    try {
+        const id = req.params.id!;
+        if (isValidPassword(req.body)) {
+            if ("oldPassword" in req.body && !await checkPassword(id, req.body.oldPassword!))
+                throw new BadRequestError("Old password incorrect");
+            else {
+                const hash = await encryptPassword(req.body.password);
+                await data.update("user", id, {password: hash});
             }
-            res.status(http.OK).send();
         }
-        catch (error) {
-            if (error instanceof data.LookupError)
-                throw new NotFoundError(error.message);
-            else
-                throw error;
-        }
+        res.status(http.OK).send();
+    }
+    catch (error) {
+        if (error instanceof data.LookupError)
+            throw new NotFoundError(error.message);
+        else
+            throw error;
     }
 }
 
-// Router
-
-const userRest = new UserRest();
+export async function userFind(property: string, value: string): Promise<string | null> {
+    const userId = await data.find("user", property, value);
+    if (userId)
+        return userId;
+    else
+        return null;
+}
 
 const router = express.Router();
-router.get("/", userRest.getAll.bind(userRest));
-router.get("/:id", userRest.get.bind(userRest));
-router.post("/", userRest.post.bind(userRest));
-router.put("/:id", userRest.put.bind(userRest));
-router.delete("/:id", userRest.delete.bind(userRest));
-router.post("/:id/password", userRest.setPassword.bind(userRest));
+router.get("/", userGetAll);
+router.get("/:id", userGet);
+router.post("/", userPost);
+router.put("/:id", userPut);
+router.delete("/:id", userDelete);
+router.post("/:id/password", userSetPassword);
 
 export default router;
